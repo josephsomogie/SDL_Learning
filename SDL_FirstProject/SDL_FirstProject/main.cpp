3a1f4e555b159b808368c891c64c23cdffdff15d
josephsomogie <128629560+josephsomogie@users.noreply.github.com>
2023-12-14 13:00:37 -0500

    first window

--- SDL_FirstProject/SDL_FirstProject/main.cpp
+++ SDL_FirstProject/SDL_FirstProject/main.cpp
@@ -3,7 +3,15 @@
 
 int main(int arc, char* argv[])
 {
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello World", "Successful", NULL);
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+	//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello World", "Successful", NULL);
 
 	return 0;
 }
